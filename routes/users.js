61ddbde388568ef7d2c3b63e0029e926c3b49475
muhammedsajal2020 <83337076+muhammedsajal2020@users.noreply.github.com>
2022-09-28 08:25:54 +0530

    login and session

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,14 @@ var router = express.Router();
 const productHelpers = require('../helpers/product-helpers');
 const userHelpers=require('../helpers/user-helpers')
 
+const verifyLogin=(req,res,next)=>{
+  if(req.session.loggedIn){
+    next()
+  }else{
+    res.redirect('/login')
+  }
+}
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -16,8 +24,12 @@ router.get('/', function(req, res, next) {
  
 });
 
-router.get('/login', function(req, res, next) {
-  res.render('login',{admin:true});
+router.get('/login',(req, res, next)=>{
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else
+  res.render('login',{"loginErr":req.session.loginErr});
+  req.session.loginErr=false
 });
 router.get('/userresg', function(req, res, next) {
   res.render('userregister',{admin:false});
@@ -38,6 +50,7 @@ router.post('/login',(req, res, next)=>{
       res.redirect('/')
     }
     else{
+      req.session.loginErr="invalid user name or password"
       res.redirect('/login')
     }
 
@@ -48,4 +61,11 @@ router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
 })
+router.get('/cart',(req,res)=>{
+  res.render('user/cart')
+})
+router.get('/account',verifyLogin,(req,res)=>{
+  res.render('user/profile')
+})
+
 module.exports = router;
